006fe21f152298ca3d3f59e5f419470b37254e58
HoroTW <horo@horotw.com>
2025-09-10 21:57:05 +0200

    Dockerize the app

--- tracker-app/backend/models.py
+++ tracker-app/backend/models.py
@@ -2,8 +2,9 @@ from sqlalchemy import create_engine, Column, Integer, String, Float, Date, Time
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import datetime
+import os
 
-DATABASE_URL = "sqlite:///./database.db"
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./data/database.db")
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
